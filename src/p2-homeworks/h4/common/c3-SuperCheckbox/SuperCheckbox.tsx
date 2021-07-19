f39d7aa63e5735a86097132323809b3597b13748
Olga <belaya-o.v@yandex.by>
2021-07-19 17:20:15 +0300

    04

--- src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -21,12 +21,14 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // сделайте так чтоб работал onChange и onChangeChecked
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)
+        onChange && onChange(e)
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
 
     return (
-        <label>
+        <label className={s.label}>
             <input
                 type={'checkbox'}
                 onChange={onChangeCallback}
