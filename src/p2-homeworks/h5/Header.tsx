317c13cb8e70b53d97e51b8b6bd8977b350a0616
Olga <belaya-o.v@yandex.by>
2021-07-27 13:44:18 +0300

    05

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {PATH} from "./Routes";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.navbar}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.navlink} activeClassName={s.active}>pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR}  className={s.navlink} activeClassName={s.active}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}  className={s.navlink} activeClassName={s.active}>junior-plus</NavLink>
 
+            <img src={'https://thypix.com/wp-content/uploads/blue-arrow-71.png'} className={s.arrow}/>
         </div>
     )
 }
